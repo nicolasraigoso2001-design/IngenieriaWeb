39d8e3a8462114648ef3a1247c2c54a1e421747a
Nicolas Raigoso <nicolasraigoso2001@gmail.com>
2026-05-18 13:42:19 +0000

    final azure frontend deploy

--- backend/startup.sh
+++ backend/startup.sh
@@ -2,6 +2,7 @@
 
 PORT=${PORT:-8000}
 
-echo "Iniciando API de Turismo en puerto $PORT"
+echo "Iniciando API en puerto $PORT"
 
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
+
