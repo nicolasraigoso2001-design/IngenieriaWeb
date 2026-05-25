228ee2bed33844d9836017a70859a38c6c0fa01a
Nicolas Raigoso <nicolasraigoso2001@gmail.com>
2026-05-25 03:56:44 +0000

    Force reseed Azure DB

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ from fastapi import Request
 
 from database.connection import engine, Base
 Base.metadata.create_all(bind=engine)
-
+import database.seedgit add .
 
 from typing import List, Optional
 from datetime import datetime, timedelta
@@ -40,7 +40,8 @@ from database.models import (
 
 # ------------------- CONFIG -------------------
 config = AppConfig()
-
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title=config.API_NAME,
     version=config.VERSION,
